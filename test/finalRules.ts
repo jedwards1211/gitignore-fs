0ea55143a1110b20ce421904a2dc00020be9a617
Andy Edwards <jedwards@fastmail.com>
2021-06-10 15:53:58 -0500

    test: improve final rules test

---
+++ test/finalRules.ts
@@ -0,0 +1,46 @@
+import declareTest from './declareTest'
+import dedent from 'dedent-js'
+
+declareTest('finalRules', {
+  files: {
+    project: {
+      '.git': {
+        HEAD: '',
+      },
+      lib: {
+        'test.js': '',
+      },
+      src: {
+        'test.js': '',
+        '.gitignore': dedent`
+          *.js
+          dwmm.js
+        `,
+        subdir: {
+          'dwmm.js': '',
+          'stuff.txt': '',
+        },
+      },
+      '.DS_Store': '',
+      'stuff.txt': '',
+      '.gitignore': dedent`
+        **/*.js
+      `,
+    },
+    '.gitignore_global': dedent`
+      .DS_Store
+      stuff.txt
+    `,
+  },
+  finalRules: ['!.DS_Store', '!dwmm.js'],
+  coreExcludesFile: '.gitignore_global',
+  expectIncludes: [
+    '.gitignore_global',
+    'project/.DS_Store',
+    'project/.gitignore',
+    'project/lib/',
+    'project/src/',
+    'project/src/.gitignore',
+    'project/src/subdir/dwmm.js',
+  ],
+})
